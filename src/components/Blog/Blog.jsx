ae9dcb4a2094dfdf52f2e5633d93724fa0901929
Rana-Sheikh <ranasheikhsbd@gmail.com>
2023-03-31 07:36:33 +0600

    added font-family & font awesome icon

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookmark } from '@fortawesome/free-solid-svg-icons';
 
 const Blog = ({blog}) => {
     console.log(blog)
     const {cover_url, authorImage, authorName, publish_date, readTime, blogTitle, hashTag } = blog;
     return (
         <div>
-            <img className='w-full h-[450px] rounded-lg' src={cover_url} alt="" />
+            <img className='w-full h-[450px] rounded-lg mb-6' src={cover_url} alt="" />
             <div className='flex justify-between items-center my-6'>
                 <div className='flex items-center gap-5'>
                     <img className='ring-2 ring-gray-700 w-[60px] h-[60px] rounded-full' src={authorImage} alt="" />
@@ -14,15 +16,18 @@ const Blog = ({blog}) => {
                         <p>{publish_date}</p>
                     </div>
                 </div>
-                <p>{readTime} min red</p>
+                <p>
+                    {readTime} min red &nbsp;
+                    <FontAwesomeIcon icon={faBookmark} />
+                </p>
             </div>
-            <h1 className='text-4xl w-3/4 font-bold my-5'>{blogTitle}</h1>
+            <h1 className='text-4xl w-3/4 font-bold my-5 leading-[50px]'>{blogTitle}</h1>
             <p className='my-4'>
                 {
                     hashTag.map(tag => <span className='mr-3'>#{tag}</span>)
                 }
             </p>
-            <p className='font-bold text-lg hover:text-rose-700 cursor-pointer text-rose-500 underline'>Mark as read</p>
+            <p className='inline-block font-bold text-lg hover:text-rose-700 cursor-pointer text-rose-500 underline'>Mark as read</p>
             <hr className='my-8' />
         </div>
     );
