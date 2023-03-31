20114fc073dc4d1d99b112ba481c564ae50c862f
Rana-Sheikh <ranasheikhsbd@gmail.com>
2023-03-31 09:43:13 +0600

    added react-tostify and sweetalert2 when clicking on the bookmark icon

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,55 +1,61 @@
-import React, { useEffect, useState } from 'react';
-import Blog from '../Blog/Blog';
-import Sidemark from '../Sidemark/Sidemark';
+import React, { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
+import Sidemark from "../Sidemark/Sidemark";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([])
-    const [spentTime, setSpentTime] = useState(0)
-    const [bookmark, setBookmark] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [spentTime, setSpentTime] = useState(0);
+  const [bookmark, setBookmark] = useState([]);
 
-    useEffect(() => {
-        fetch('data.json')
-        .then(res => res.json())
-        .then(data => setBlogs(data))
-    }, [])
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
 
-    const handleSpentTime = (readTime) => {
-        const previousReadTime = +readTime;
-        const newReadTime = +spentTime;
-        setSpentTime(previousReadTime + newReadTime)
+  const handleSpentTime = (readTime) => {
+    const previousReadTime = +readTime;
+    const newReadTime = +spentTime;
+    setSpentTime(previousReadTime + newReadTime);
+  };
+
+  const handleBookmark = (blog) => {
+    const exists = bookmark?.find((b) => b?.id === blog.id);
+    if (!exists) {
+      setBookmark([...bookmark, blog]);
+      toast.success("Successfully Bookmarked !!", {
+        position: toast.POSITION.TOP_LEFT,
+      });
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Oops..",
+        text: "You Have Already Bookmarked This Blog!",
+      });
     }
+  };
 
-    const handleBookmark = (blog) => {
-        const exists = bookmark?.find(b => b?.id === blog.id);
-        if(!exists){
-            setBookmark([...bookmark, blog])
-        }
-        else{
-            alert('Kire bhai aita to akbar add hoise')
-        }
-    }
+  return (
+    <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-6">
+      <div className="md:col-span-2">
+        {blogs.map((blog) => (
+          <Blog
+            key={blog.id}
+            blog={blog}
+            handleSpentTime={handleSpentTime}
+            handleBookmark={handleBookmark}
+          ></Blog>
+        ))}
+      </div>
 
-    return (
-        <div className='container mx-auto grid grid-cols-1 md:grid-cols-3 gap-6'>
-            <div className='md:col-span-2'>
-                {
-                    blogs.map(blog => <Blog
-                    key={blog.id}
-                    blog={blog}
-                    handleSpentTime={handleSpentTime}
-                    handleBookmark={handleBookmark}
-                    ></Blog>)
-                }
-            </div>
-
-            <div>
-                <Sidemark 
-                spentTime={spentTime}
-                bookmark={bookmark}
-                ></Sidemark>
-            </div>
-        </div>
-    );
+      <div>
+        <Sidemark spentTime={spentTime} bookmark={bookmark}></Sidemark>
+      </div>
+      <ToastContainer></ToastContainer>
+    </div>
+  );
 };
 
 export default Home;
