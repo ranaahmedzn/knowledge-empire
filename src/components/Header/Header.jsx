ef6abcde94d5d841f8226c2a281265ddd38ff80d
Rana-Sheikh <ranasheikhsbd@gmail.com>
2023-04-06 11:00:07 +0600

    added hamburger menu in navbar for mobile device

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/logo.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faClose, faRightToBracket } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <nav className="container mx-auto mt-5 mb-8">
       <div className="navbar">
@@ -20,7 +22,7 @@ const Header = () => {
                     Login
                 </a>
             </div>
-          <div className="dropdown dropdown-end">
+            <div className="hidden md:block dropdown dropdown-end">
             <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
               <div className="w-12 rounded-full">
                 <img src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" />
@@ -43,7 +45,25 @@ const Header = () => {
                 <a>Logout</a>
               </li>
             </ul>
-          </div>
+            </div>
+
+            {/* for mobile device only */}
+            <div className="md:hidden w-fit h-fit" onClick={() => setIsOpen(!isOpen)}>
+              <ul className={`${isOpen ? "top-24 left-0" : "-top-96 left-0"} duration-500 absolute px-5 py-8 bg-indigo-500 text-slate-100 w-full flex flex-col gap-6`}>
+                  <li><a href="#" className="text-lg font-bold hover:border-b-4 p-1 border-indigo-500 transition-all duration-75">Home</a></li>
+                  <li><a href="#" className="text-lg font-bold hover:border-b-4 p-1 border-indigo-500 transition-all duration-75">Blog</a></li>
+                  <li><a href="#" className="text-lg font-bold hover:border-b-4 p-1 border-indigo-500 transition-all duration-75">About</a></li>
+                  <li><a href="#" className="text-lg font-bold hover:border-b-4 p-1 border-indigo-500 transition-all duration-75">
+                      <FontAwesomeIcon icon={faRightToBracket} />&nbsp;
+                      Login
+                  </a></li>
+              </ul>
+              {
+                isOpen ?
+                <FontAwesomeIcon className="text-3xl" icon={faClose} /> :
+                <FontAwesomeIcon className="text-3xl" icon={faBars} />
+              }
+            </div>
         </div>
       </div>
       <hr />
